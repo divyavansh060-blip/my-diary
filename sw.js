570866fe27270863d5eac08e1af4c094bcc96b8d
divya <282848370+divyavansh060-blip@users.noreply.github.com>
2026-08-12 12:02:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,104 +1,39 @@
-const CACHE_NAME = 'my-diary-v1';
+const CACHE_NAME = "my-diary-v1";
 
-const APP_FILES = [
-    './',
-    './index.html',
-    './manifest.json',
-    './sw.js',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
-
+self.addEventListener("install", event => {
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-        .then(cache => {
-
-            return cache.addAll(APP_FILES);
-
-        })
-
+            .then(cache => cache.addAll(FILES_TO_CACHE))
     );
 
     self.skipWaiting();
-
 });
 
-
-self.addEventListener('activate', event => {
-
+self.addEventListener("activate", event => {
     event.waitUntil(
-
-        caches.keys().then(keys => {
-
-            return Promise.all(
-
+        caches.keys().then(keys =>
+            Promise.all(
                 keys
-                .filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-
-            );
-
-        })
-
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
     );
 
     self.clients.claim();
-
 });
 
-
-self.addEventListener('fetch', event => {
-
+self.addEventListener("fetch", event => {
     event.respondWith(
-
         caches.match(event.request)
-        .then(cachedResponse => {
-
-            if (cachedResponse) {
-
-                return cachedResponse;
-
-            }
-
-            return fetch(event.request)
-            .then(response => {
-
-                if (
-                    !response ||
-                    response.status !== 200 ||
-                    response.type === 'opaque'
-                ) {
-
-                    return response;
-
-                }
-
-                const responseClone = response.clone();
-
-                caches.open(CACHE_NAME)
-                .then(cache => {
-
-                    cache.put(
-                        event.request,
-                        responseClone
-                    );
-
-                });
-
-                return response;
-
-            })
-            .catch(() => {
-
-                return caches.match('./index.html');
-
-            });
-
-        })
-
+            .then(cached => cached || fetch(event.request))
     );
-
 });
