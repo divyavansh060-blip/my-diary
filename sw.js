5e0b550192281ced634b08aa86b0eda2b24f42ed
divya <282848370+divyavansh060-blip@users.noreply.github.com>
2026-08-12 11:48:12 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,104 @@
+const CACHE_NAME = 'my-diary-v1';
+
+const APP_FILES = [
+    './',
+    './index.html',
+    './manifest.json',
+    './sw.js',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            return cache.addAll(APP_FILES);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+
+self.addEventListener('activate', event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(cachedResponse => {
+
+            if (cachedResponse) {
+
+                return cachedResponse;
+
+            }
+
+            return fetch(event.request)
+            .then(response => {
+
+                if (
+                    !response ||
+                    response.status !== 200 ||
+                    response.type === 'opaque'
+                ) {
+
+                    return response;
+
+                }
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME)
+                .then(cache => {
+
+                    cache.put(
+                        event.request,
+                        responseClone
+                    );
+
+                });
+
+                return response;
+
+            })
+            .catch(() => {
+
+                return caches.match('./index.html');
+
+            });
+
+        })
+
+    );
+
+});
